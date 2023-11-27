c08fec1947745ad7e5369c924e93ebf02f3da25c
Иван Былёв <ivan.bylyov@gmail.com>
2023-11-27 18:06:47 +0300

    Initial commit

---
+++ app/src/main/java/com/vortex/android/vortexchat/activities/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.vortex.android.vortexchat.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vortex.android.vortexchat.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
---
+++ app/src/main/java/com/vortex/android/vortexchat/ui/sign_up/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.vortex.android.vortexchat.ui.sign_up
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
